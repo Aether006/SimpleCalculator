b09485d4868ed034f9268977460830687ad16672
Thomas Woolley <thomaswoolley22@icloud.com>
2024-07-09 13:01:15 +0100

    New Changes
    All Operators added and other functions,
    Now work on error handling and exeptions

--- SimpleCalculator/Form1.Designer.cs
+++ SimpleCalculator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SimpleCalculator
 {
-    partial class Form1
+    partial class Calculator
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,79 +28,337 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            textBox1 = new TextBox();
+            Clear = new Button();
+            NaturalLog = new Button();
+            Sqrt = new Button();
+            Add = new Button();
+            Output = new TextBox();
+            Seven = new Button();
+            Eight = new Button();
+            Nine = new Button();
+            Divide = new Button();
+            Multiply = new Button();
+            Six = new Button();
+            Five = new Button();
+            Four = new Button();
+            One = new Button();
+            Two = new Button();
+            Three = new Button();
+            Subtract = new Button();
+            Equals = new Button();
+            Zero = new Button();
+            Point = new Button();
+            Pi = new Button();
+            Power = new Button();
+            button5 = new Button();
             SuspendLayout();
             // 
-            // button1
+            // Clear
             // 
-            button1.Location = new Point(16, 132);
-            button1.Name = "button1";
-            button1.Size = new Size(65, 54);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            Clear.Location = new Point(16, 132);
+            Clear.Name = "Clear";
+            Clear.Size = new Size(65, 54);
+            Clear.TabIndex = 0;
+            Clear.Text = "AC";
+            Clear.UseVisualStyleBackColor = true;
+            Clear.Click += Clear_Click;
             // 
-            // button2
+            // NaturalLog
             // 
-            button2.Location = new Point(87, 132);
-            button2.Name = "button2";
-            button2.Size = new Size(65, 54);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            NaturalLog.Location = new Point(87, 132);
+            NaturalLog.Name = "NaturalLog";
+            NaturalLog.Size = new Size(65, 54);
+            NaturalLog.TabIndex = 1;
+            NaturalLog.Text = "ln";
+            NaturalLog.UseVisualStyleBackColor = true;
+            NaturalLog.Click += NaturalLog_Click;
             // 
-            // button3
+            // Sqrt
             // 
-            button3.Location = new Point(158, 132);
-            button3.Name = "button3";
-            button3.Size = new Size(65, 54);
-            button3.TabIndex = 2;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
+            Sqrt.Location = new Point(158, 132);
+            Sqrt.Name = "Sqrt";
+            Sqrt.Size = new Size(65, 54);
+            Sqrt.TabIndex = 2;
+            Sqrt.Text = "Sqrt";
+            Sqrt.UseVisualStyleBackColor = true;
+            Sqrt.Click += Sqrt_Click;
             // 
-            // button4
+            // Add
             // 
-            button4.Location = new Point(229, 132);
-            button4.Name = "button4";
-            button4.Size = new Size(65, 54);
-            button4.TabIndex = 3;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
+            Add.Location = new Point(229, 312);
+            Add.Name = "Add";
+            Add.Size = new Size(65, 54);
+            Add.TabIndex = 3;
+            Add.Text = "+";
+            Add.UseVisualStyleBackColor = true;
+            Add.Click += Add_Click;
             // 
-            // textBox1
+            // Output
             // 
-            textBox1.Location = new Point(16, 68);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(278, 23);
-            textBox1.TabIndex = 4;
-            textBox1.TextChanged += textBox1_TextChanged;
+            Output.BackColor = SystemColors.ControlLightLight;
+            Output.Font = new Font("Symbol", 24F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            Output.Location = new Point(16, 44);
+            Output.Name = "Output";
+            Output.ReadOnly = true;
+            Output.Size = new Size(278, 47);
+            Output.TabIndex = 4;
+            Output.Text = "0";
+            Output.TextAlign = HorizontalAlignment.Right;
+            Output.TextChanged += textBox1_TextChanged;
             // 
-            // Form1
+            // Seven
+            // 
+            Seven.Location = new Point(16, 192);
+            Seven.Name = "Seven";
+            Seven.Size = new Size(65, 54);
+            Seven.TabIndex = 5;
+            Seven.Text = "7";
+            Seven.UseVisualStyleBackColor = true;
+            Seven.Click += Seven_Click;
+            // 
+            // Eight
+            // 
+            Eight.Location = new Point(87, 192);
+            Eight.Name = "Eight";
+            Eight.Size = new Size(65, 54);
+            Eight.TabIndex = 6;
+            Eight.Text = "8";
+            Eight.UseVisualStyleBackColor = true;
+            Eight.Click += Eight_Click;
+            // 
+            // Nine
+            // 
+            Nine.Location = new Point(158, 192);
+            Nine.Name = "Nine";
+            Nine.Size = new Size(65, 54);
+            Nine.TabIndex = 7;
+            Nine.Text = "9";
+            Nine.UseVisualStyleBackColor = true;
+            Nine.Click += Nine_Click;
+            // 
+            // Divide
+            // 
+            Divide.Location = new Point(229, 132);
+            Divide.Name = "Divide";
+            Divide.Size = new Size(65, 54);
+            Divide.TabIndex = 8;
+            Divide.Text = "/";
+            Divide.UseVisualStyleBackColor = true;
+            Divide.Click += Divide_Click;
+            // 
+            // Multiply
+            // 
+            Multiply.Location = new Point(229, 192);
+            Multiply.Name = "Multiply";
+            Multiply.Size = new Size(65, 54);
+            Multiply.TabIndex = 9;
+            Multiply.Text = "*";
+            Multiply.UseVisualStyleBackColor = true;
+            Multiply.Click += Multiply_Click;
+            // 
+            // Six
+            // 
+            Six.Location = new Point(158, 252);
+            Six.Name = "Six";
+            Six.Size = new Size(65, 54);
+            Six.TabIndex = 10;
+            Six.Text = "6";
+            Six.UseVisualStyleBackColor = true;
+            Six.Click += Six_Click;
+            // 
+            // Five
+            // 
+            Five.Location = new Point(87, 252);
+            Five.Name = "Five";
+            Five.Size = new Size(65, 54);
+            Five.TabIndex = 11;
+            Five.Text = "5";
+            Five.UseVisualStyleBackColor = true;
+            Five.Click += Five_Click;
+            // 
+            // Four
+            // 
+            Four.Location = new Point(16, 252);
+            Four.Name = "Four";
+            Four.Size = new Size(65, 54);
+            Four.TabIndex = 12;
+            Four.Text = "4";
+            Four.UseVisualStyleBackColor = true;
+            Four.Click += Four_Click;
+            // 
+            // One
+            // 
+            One.Location = new Point(16, 312);
+            One.Name = "One";
+            One.Size = new Size(65, 54);
+            One.TabIndex = 13;
+            One.Text = "1";
+            One.UseVisualStyleBackColor = true;
+            One.Click += One_Click;
+            // 
+            // Two
+            // 
+            Two.Location = new Point(87, 312);
+            Two.Name = "Two";
+            Two.Size = new Size(65, 54);
+            Two.TabIndex = 14;
+            Two.Text = "2";
+            Two.UseVisualStyleBackColor = true;
+            Two.Click += Two_Click;
+            // 
+            // Three
+            // 
+            Three.Location = new Point(158, 312);
+            Three.Name = "Three";
+            Three.Size = new Size(65, 54);
+            Three.TabIndex = 15;
+            Three.Text = "3";
+            Three.UseVisualStyleBackColor = true;
+            Three.Click += Three_Click;
+            // 
+            // Subtract
+            // 
+            Subtract.Location = new Point(229, 252);
+            Subtract.Name = "Subtract";
+            Subtract.Size = new Size(65, 54);
+            Subtract.TabIndex = 16;
+            Subtract.Text = "-";
+            Subtract.UseVisualStyleBackColor = true;
+            Subtract.Click += Subtract_Click;
+            // 
+            // Equals
+            // 
+            Equals.Location = new Point(229, 372);
+            Equals.Name = "Equals";
+            Equals.Size = new Size(65, 114);
+            Equals.TabIndex = 17;
+            Equals.Text = "=";
+            Equals.UseVisualStyleBackColor = true;
+            Equals.Click += Equals_Click;
+            // 
+            // Zero
+            // 
+            Zero.Location = new Point(16, 372);
+            Zero.Name = "Zero";
+            Zero.Size = new Size(136, 54);
+            Zero.TabIndex = 18;
+            Zero.Text = "0";
+            Zero.UseVisualStyleBackColor = true;
+            Zero.Click += Zero_Click;
+            // 
+            // Point
+            // 
+            Point.Location = new Point(158, 372);
+            Point.Name = "Point";
+            Point.Size = new Size(65, 54);
+            Point.TabIndex = 19;
+            Point.Text = ".";
+            Point.UseVisualStyleBackColor = true;
+            Point.Click += Point_Click;
+            // 
+            // Pi
+            // 
+            Pi.Location = new Point(16, 432);
+            Pi.Name = "Pi";
+            Pi.Size = new Size(65, 54);
+            Pi.TabIndex = 20;
+            Pi.Text = "π";
+            Pi.UseVisualStyleBackColor = true;
+            Pi.Click += Pi_Click;
+            // 
+            // Power
+            // 
+            Power.Location = new Point(87, 432);
+            Power.Name = "Power";
+            Power.Size = new Size(65, 54);
+            Power.TabIndex = 21;
+            Power.Text = "^";
+            Power.UseVisualStyleBackColor = true;
+            Power.Click += Power_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(158, 432);
+            button5.Name = "button5";
+            button5.Size = new Size(65, 54);
+            button5.TabIndex = 22;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // Calculator
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(312, 440);
-            Controls.Add(textBox1);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Name = "Form1";
-            Text = "Form1";
+            AutoSize = true;
+            ClientSize = new Size(309, 502);
+            Controls.Add(button5);
+            Controls.Add(Power);
+            Controls.Add(Pi);
+            Controls.Add(Point);
+            Controls.Add(Zero);
+            Controls.Add(Equals);
+            Controls.Add(Subtract);
+            Controls.Add(Three);
+            Controls.Add(Two);
+            Controls.Add(One);
+            Controls.Add(Four);
+            Controls.Add(Five);
+            Controls.Add(Six);
+            Controls.Add(Multiply);
+            Controls.Add(Divide);
+            Controls.Add(Nine);
+            Controls.Add(Eight);
+            Controls.Add(Seven);
+            Controls.Add(Output);
+            Controls.Add(Add);
+            Controls.Add(Sqrt);
+            Controls.Add(NaturalLog);
+            Controls.Add(Clear);
+            MaximizeBox = false;
+            MaximumSize = new Size(325, 541);
+            MinimumSize = new Size(325, 541);
+            Name = "Calculator";
+            Text = "Calculator";
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private TextBox textBox1;
+        private Button Pi;
+        private Button NaturalLog;
+        private Button Sqrt;
+        private TextBox Output;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button Zero;
+        private Button Point;
+        private Button One;
+        private Button Two;
+        private Button Three;
+        private Button Clear;
+        private Button Add;
+        private Button Seven;
+        private Button Eight;
+        private Button Nine;
+        private Button Divide;
+        private Button Multiply;
+        private Button Six;
+        private Button Five;
+        private Button Four;
+        private Button Subtract;
+        private Button Equals;
+        private Button Power;
     }
 }
--- SimpleCalculator/Form1.cs
+++ SimpleCalculator/Form1.cs
@@ -1,8 +1,16 @@
+using System.Runtime.ExceptionServices;
+
 namespace SimpleCalculator
 {
-    public partial class Form1 : Form
+    public partial class Calculator : Form
     {
-        public Form1()
+        double firstNum;
+        double secondNum;
+        double ans;
+        char oper;
+        const double pi = 3.141592653;
+
+        public Calculator()
         {
             InitializeComponent();
         }
@@ -10,6 +18,236 @@ namespace SimpleCalculator
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void Zero_Click(object sender, EventArgs e)
+        {
+            Output.Text = Output.Text + "0";
+        }
+
+        private void One_Click(object sender, EventArgs e)
+        {
+            if (Output.Text == "0" && Output.Text != null)
+            {
+                Output.Text = "1";
+            }
+            else
+            {
+                Output.Text = Output.Text + "1";
+            }
+        }
+
+        private void Two_Click(object sender, EventArgs e)
+        {
+            if (Output.Text == "0" && Output.Text != null)
+            {
+                Output.Text = "2";
+            }
+            else
+            {
+                Output.Text = Output.Text + "2";
+            }
+        }
+
+        private void Three_Click(object sender, EventArgs e)
+        {
+            if (Output.Text == "0" && Output.Text != null)
+            {
+                Output.Text = "3";
+            }
+            else
+            {
+                Output.Text = Output.Text + "3";
+            }
+        }
+
+        private void Four_Click(object sender, EventArgs e)
+        {
+            if (Output.Text == "0" && Output.Text != null)
+            {
+                Output.Text = "4";
+            }
+            else
+            {
+                Output.Text = Output.Text + "4";
+            }
+        }
+
+        private void Five_Click(object sender, EventArgs e)
+        {
+            if (Output.Text == "0" && Output.Text != null)
+            {
+                Output.Text = "5";
+            }
+            else
+            {
+                Output.Text = Output.Text + "5";
+            }
+        }
+
+        private void Six_Click(object sender, EventArgs e)
+        {
+            if (Output.Text == "0" && Output.Text != null)
+            {
+                Output.Text = "6";
+            }
+            else
+            {
+                Output.Text = Output.Text + "6";
+            }
+        }
+
+        private void Seven_Click(object sender, EventArgs e)
+        {
+            if (Output.Text == "0" && Output.Text != null)
+            {
+                Output.Text = "7";
+            }
+            else
+            {
+                Output.Text = Output.Text + "7";
+            }
+        }
+
+        private void Eight_Click(object sender, EventArgs e)
+        {
+            if (Output.Text == "0" && Output.Text != null)
+            {
+                Output.Text = "8";
+            }
+            else
+            {
+                Output.Text = Output.Text + "8";
+            }
+        }
+
+        private void Nine_Click(object sender, EventArgs e)
+        {
+            if (Output.Text == "0" && Output.Text != null)
+            {
+                Output.Text = "9";
+            }
+            else
+            {
+                Output.Text = Output.Text + "9";
+            }
+        }
+
+        private void Multiply_Click(object sender, EventArgs e)
+        {
+            firstNum = Convert.ToDouble(Output.Text);
+            Output.Clear();
+            oper = '*';
+        }
+
+        private void Point_Click(object sender, EventArgs e)
+        {
+            Output.Text += ".";
+        }
+
+
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            firstNum = Convert.ToDouble(Output.Text);
+            Output.Clear();
+            oper = '+';
+        }
+
+        private void Subtract_Click(object sender, EventArgs e)
+        {
+            firstNum = Convert.ToDouble(Output.Text);
+            Output.Clear();
+            oper = '-';
+        }
+
+        private void Divide_Click(object sender, EventArgs e)
+        {
+            firstNum = Convert.ToDouble(Output.Text);
+            Output.Clear();
+            oper = '/';
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Output.Clear();
+            Output.Text = "0";
+        }
+
+
+
+
+
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Equals_Click(object sender, EventArgs e)
+        {
+            secondNum = Convert.ToDouble(Output.Text);
+            Output.Clear();
+
+            //if (secondNum == 0 && oper == '/')
+            //{
+            //    Output.Text = "MathErr";
+            //}
+            switch (oper)
+            {
+                default:
+
+                    break;
+
+                case '*':
+                    Output.Text = (firstNum * secondNum).ToString();
+                    break;
+
+                case '/':
+
+                    Output.Text = (firstNum / secondNum).ToString();
+                    break;
+
+                case '+':
+                    Output.Text = (firstNum + secondNum).ToString();
+                    break;
+
+                case '-':
+                    Output.Text = (firstNum - secondNum).ToString();
+                    break;
+
+                case '^':
+                    Output.Text = (Math.Pow(firstNum,secondNum)).ToString();
+                break;
+
+
+            }
+        }
+
+        private void NaturalLog_Click(object sender, EventArgs e)
+        {
+            firstNum = Convert.ToDouble(Output.Text);
+            Output.Clear();
+            Output.Text = Math.Log(firstNum).ToString();
+        }
+
+        private void Sqrt_Click(object sender, EventArgs e)
+        {
+            firstNum = Convert.ToDouble(Output.Text);
+            Output.Clear();
+            Output.Text = Math.Sqrt(firstNum).ToString();
+        }
+
+        private void Pi_Click(object sender, EventArgs e)
+        {
+            Output.Text = pi.ToString();
+        }
+
+
+        private void Power_Click(object sender, EventArgs e)
+        {
+            firstNum = Convert.ToDouble(Output.Text);
+            Output.Clear();
+            oper = '^';
         }
     }
 }
